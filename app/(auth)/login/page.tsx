c07584cef809a931f5b38ea18849c29b2f72d1e8
oreoluwa <akintomowooreoluwa@gmail.com>
2025-01-12 18:57:35 +0100

    create database schema

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -15,23 +15,27 @@ const Auth = () => {
 
   return (
     <div className="relative w-full h-full  bg-[url('/image/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover ">
-      <div className="md:bg-black/50 h-full w-full bg-black">
+      <div className="lg:bg-black/50 h-full w-full bg-black">
         <nav className="px-12 py-5">
           <img src="/image/logo.png" alt="Netflix Logo" className="h-10 " />
         </nav>
         <div className="flex justify-center">
-          <div className="md:bg-black/70  px-16 py-16 self-center mt-2 md:w-2/5 md:max-w-md rounded-md w-full">
-            <h1 className="text-white text-4xl mb-8 font-semibold">Sign in</h1>
+          <div className="lg:bg-black/70  px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h1 className="text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign in" : "Register"}
+            </h1>
             <div className="flex flex-col gap-4">
-              <Input
-                label="Username"
-                onChange={(e) => {
-                  setUsername(e.target.value);
-                }}
-                id="username"
-                type="text"
-                value={username}
-              />
+              {variant === "signup" && (
+                <Input
+                  label="Username"
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                  }}
+                  id="username"
+                  type="text"
+                  value={username}
+                />
+              )}
               <Input
                 label="Email"
                 onChange={(e) => {
@@ -52,16 +56,18 @@ const Auth = () => {
               />
             </div>
             <button className="bg-red-600 text-white rounded-md py-2 mt-10 hover:bg-red-700 transition w-full">
-              Login
+              {variant === "login" ? "Sign in" : "Sign up"}
             </button>
 
             <p className="text-neutral-500 mt-12">
-              First time using Netflix?
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
               <span
                 onClick={toggleVariant}
                 className="text-white ml-1 hover:underline cursor-pointer"
               >
-                Create an account
+                {variant === "login" ? "Create an account" : "Sign in"}
               </span>
             </p>
           </div>
